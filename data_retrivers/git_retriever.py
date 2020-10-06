bd697a6a289683439fbbc7b45345e10fecace555
Alex Incerti <alex.incerti@outlook.com>
2020-10-06 20:09:44 +0200

    Tag comparison downloading + parsing

--- data_retrivers/git_retriever.py
+++ data_retrivers/git_retriever.py
@@ -1,11 +1,14 @@
 import requests
 import pprint
 import pickle
+import pandas as pd
 from concurrent.futures import ThreadPoolExecutor, wait, ALL_COMPLETED
 pp = pprint.PrettyPrinter(depth=6)
 
 _BASE_URL = "https://api.github.com/"
-GITHUB_TOKEN = "e0776a1059e8702da7e1ee7ad3facd806374bd07"
+
+with open(".githubtoken", "r") as f:
+    GITHUB_TOKEN = f.read()
 
 headers = { \
     "Content-Type": "application/json", \
@@ -32,6 +35,7 @@ def _request(method="get", endpoint="", headers=headers, **kwargs):
 def get_compare_tags(owner, repo, tag1, tag2):
     query = f"repos/{owner}/{repo}/compare/{tag1}...{tag2}"
     response = _request(endpoint=query)
+    checkRateLimit(response)
     return response
 
 def get_pull_request(owner, repo, pull_number):
@@ -42,6 +46,7 @@ def get_pull_request(owner, repo, pull_number):
 def get_pull_request_commits(owner, repo, pull_number):
     query = f"repos/{owner}/{repo}/pulls/{pull_number}/commits?per_page=1000"
     response = _request(endpoint=query)
+    checkRateLimit(response)
     return response
 
 def get_pull_request_data(owner, repo, pull_number):
@@ -72,14 +77,14 @@ if __name__ == "__main1__":
     res = get_pull_request_data("SonarSource", "sonarqube", 1808)
     pp.pprint(res)
 
-PR_OFFSET = 1401
+PR_OFFSET = 3201
 
 #Pull requests retriver
-if __name__ == "__main__":
+if __name__ == "__main1__":
     with ThreadPoolExecutor() as executor:
         data = list()
         futures = set()
-        for i in range(PR_OFFSET, 3250):
+        for i in range(PR_OFFSET, 3251):
             futures.add(executor.submit(get_pull_request_data, "SonarSource", "sonarqube", i))
             if(i%50 == 0):
                 completed, futures = wait(futures, return_when=ALL_COMPLETED)
@@ -91,15 +96,103 @@ if __name__ == "__main__":
                 data = list()
                 print(f"Downloaded {i} pull requests...")
 
-    #Handle the last ones
-    completed, futures = wait(futures, return_when=ALL_COMPLETED)
-    for future in completed:
-        data.append(future.result())
-    with open(f"pull_requests/pull_requests{i}.pkl", "wb") as f:
-            pickle.dump(data, f)
-    """shas = []
-    msgshas = []
-    for commit in response:
-        shas.append(commit["sha"])
-        msgshas.append((commit["commit"]["message"], commit["sha"]))
-    pp.pprint(shas)"""
+    if(i%50 != 0):
+        #Handle the last ones
+        completed, futures = wait(futures, return_when=ALL_COMPLETED)
+        for future in completed:
+            data.append(future.result())
+        with open(f"pull_requests/pull_requests{i}.pkl", "wb") as f:
+                pickle.dump(data, f)
+
+if __name__ == "__main2__":
+    tag_dict = {
+        "5.4-M7": "5.4-M6",
+        "5.4-M8": "5.4-M7",
+        "6.3": "6.3.0.18800"
+    }
+    tagsDf = pd.read_csv("csv/tags.csv", index_col=0) 
+    comparisons = []
+    i = 0
+    for index, row in tagsDf.sort_values(by=["Tag_number", "Date"]).iterrows():
+        head_tag = row.Tag
+        if head_tag in tag_dict:
+            base_tag = tag_dict[row.Tag]
+        elif i != 0:
+            base_tag = previousTag
+        if i != 0:
+            response = get_compare_tags("SonarSource", "sonarqube", base_tag, head_tag)
+            comparisons.append(response.json())
+            print(f"Comparing {base_tag} and {head_tag}: {response.status_code}...")
+        previousTag = row.Tag
+        if(i%50 == 0):
+            print(f"Downloaded {i} comparisons...")
+        i += 1
+    with open(f"pkl/compare_tags.pkl", "wb") as f:
+        pickle.dump(comparisons, f)
+
+if __name__ == "__main__":
+    import os.path
+    from os import path
+    tagsDf = pd.read_csv("csv/tags.csv", index_col=0)
+    BACKWARDS_TRIES = 10
+    FORWARD_TRIES = 5
+    folder = "pkl/compare_tags"
+
+    #Load file with already tried combinations
+    already_tried_tags_versions = []
+    tried_combinations = ""
+    if path.exists(f"{folder}/compare_tags_combination_log.txt"):
+        with open(f"{folder}compare_tags_combination_log.txt", "r") as a_file:
+            for line in a_file:
+                stripped_line = line.strip()
+                tried_combinations = tried_combinations + stripped_line + "\n" 
+                already_tried_tags_versions.append(stripped_line)
+
+    comparisons = []
+    with ThreadPoolExecutor() as executor:
+        tags = list(tagsDf.sort_values(by=["Tag_number", "Date"]).Tag)
+        futures = set()
+        for i, head_tag in enumerate(tags):
+            #Backwards
+            start_index = 0 if i-BACKWARDS_TRIES < 0 else i-BACKWARDS_TRIES
+            for j in range(start_index, i):
+                if f"{tags[j]}:{head_tag}" in already_tried_tags_versions:
+                    continue
+                futures.add(executor.submit(get_compare_tags, "SonarSource", "sonarqube", tags[j], head_tag))  
+                print(f"Comparing {tags[j]} and {head_tag}...")
+                tried_combinations = tried_combinations + f"{tags[j]}:{head_tag}\n"
+            #Forward
+            end_index = len(tags) if i+FORWARD_TRIES > len(tags) else i+FORWARD_TRIES
+            if i == len(tags):
+                continue
+            for j in range(i+1, end_index):
+                if f"{tags[j]}:{head_tag}" in already_tried_tags_versions:
+                    continue
+                futures.add(executor.submit(get_compare_tags, "SonarSource", "sonarqube", tags[j], head_tag))  
+                print(f"Comparing {tags[j]} and {head_tag}...")
+                tried_combinations = tried_combinations + f"{tags[j]}:{head_tag}\n"
+            completed, futures = wait(futures, return_when=ALL_COMPLETED)
+            for future in completed:
+                response = future.result()
+                if response.status_code != 200:
+                    print(f"Failed call with {response.status_code} code: {response.status_code}")
+                #empty commits
+                response = response.json()
+                response["commits"] = []
+                comparisons.append(response)
+            futures = set()
+            
+            if len(comparisons) == 0:
+                continue
+
+            with open(f"{folder}/compare_tags_combination_log.txt", "a") as f:
+                f.write(tried_combinations)
+                tried_combinations = ""   
+            print(f"Saving {len(comparisons)} comparisons...")
+            with open(f"{folder}/compare_tags/compare_tags_combination{i}.pkl", "wb") as f:
+                pickle.dump(comparisons, f)
+                comparisons = []
+
+if __name__ == "__main2__":
+     response = get_compare_tags("SonarSource", "sonarqube", "4.1.2", "4.2")
+     print(response)
